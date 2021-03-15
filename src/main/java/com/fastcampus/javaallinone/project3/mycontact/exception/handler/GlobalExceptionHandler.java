cc3804683b4885a9980e1482554c9898dd463b89
JeongEun <lisasje25@gmail.com>
2021-03-16 01:35:58 +0900

    parameter validator

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.fastcampus.javaallinone.project3.mycontact.exception.RenameNotPermitt
 import com.fastcampus.javaallinone.project3.mycontact.exception.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,7 +25,13 @@ public class GlobalExceptionHandler {
         return ErrorResponse.of(HttpStatus.BAD_REQUEST,ex.getMessage());
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleRuntimeException(RuntimeException ex){
         log.error("서버 오류 : {}",ex.getMessage(),ex);
